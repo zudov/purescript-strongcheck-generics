3498943d498b6fe7635579a52497ea2f8c0fd2bf
Konstantin Zudov <co@zudov.me>
2017-02-06 00:06:19 +0200

    Cleanup `genGenericSpine`, `genGenericSignature`
    A byproduct from the attempts to find what goes wrong there.
    Should slightly help with further work on #5.

--- src/Test/StrongCheck/Generic.purs
+++ src/Test/StrongCheck/Generic.purs
@@ -11,24 +11,26 @@ module Test.StrongCheck.Generic
   ) where
 
 import Prelude
-
 import Control.Plus (empty)
-
-import Data.Array (nub, uncons, zipWith, length, filter, (:))
-import Data.Foldable (class Foldable, foldMap)
-import Data.Generic (class Generic, GenericSignature(..), GenericSpine(..), isValidSpine, toSpine, toSignature, fromSpine)
+import Data.Array (filter, length, nub, uncons, zipWith, (:))
+import Data.Foldable (class Foldable, fold, foldMap)
+import Data.Generic
+  ( class Generic, GenericSignature(..), GenericSpine(..), DataConstructor
+  , fromSpine, isValidSpine, toSignature, toSpine
+  )
 import Data.Int (toNumber)
 import Data.List (fromFoldable)
-import Data.Maybe (Maybe(..), maybe, fromJust)
+import Data.Maybe (Maybe(Nothing, Just), fromJust, maybe)
+import Data.Monoid (mempty)
 import Data.Monoid.Endo (Endo(..))
-import Data.Traversable (for, traverse)
+import Data.Profunctor.Strong (second)
+import Data.Traversable (for)
 import Data.Tuple (Tuple(..))
-
 import Partial.Unsafe (unsafePartial)
-
-import Test.StrongCheck.Arbitrary (class Arbitrary, arbitrary, coarbitrary)
-import Test.StrongCheck.Gen (Gen, Size, frequency, arrayOf, oneOf, resize, sized, elements)
-
+import Test.StrongCheck.Arbitrary
+  ( class Arbitrary, arbitrary, coarbitrary )
+import Test.StrongCheck.Gen
+  ( Gen, Size, arrayOf, elements, frequency, oneOf, resize, sized )
 import Type.Proxy (Proxy(..))
 
 -- | Generate arbitrary values for any `Generic` data structure
@@ -40,15 +42,15 @@ gCoarbitrary :: forall a r. Generic a => a -> Gen r -> Gen r
 gCoarbitrary = go <<< toSpine
   where
     go :: GenericSpine -> Gen r -> Gen r
-    go (SArray ss) = applyAll (map (go <<< (_ $ unit)) ss)
-    go (SBoolean b) = coarbitrary b
-    go (SString s) = coarbitrary s
-    go (SChar c) = coarbitrary c
-    go (SInt i) = coarbitrary i
-    go (SNumber n) = coarbitrary n
-    go (SRecord fs) = applyAll (map (\f -> coarbitrary f.recLabel <<< go (f.recValue unit)) fs)
-    go (SProd ctor ss) = coarbitrary ctor <<< applyAll (map (go <<< (_ $ unit)) ss)
-    go SUnit = coarbitrary unit
+    go (SArray ss)     = applyAll (map (go <<< (_ $ unit)) ss)
+    go (SBoolean b)    = coarbitrary b
+    go (SString s)     = coarbitrary s
+    go (SChar c)       = coarbitrary c
+    go (SInt i)        = coarbitrary i
+    go (SNumber n)     = coarbitrary n
+    go (SRecord fs)    = applyAll (map (\f -> coarbitrary f.recLabel <<< go (f.recValue unit)) fs)
+    go (SProd ctor ss) = coarbitrary ctor <<< applyAll (map (go <<< force) ss)
+    go SUnit           = coarbitrary unit
 
 applyAll :: forall f a. Foldable f => f (a -> a) -> a -> a
 applyAll = (case _ of Endo a -> a) <<< foldMap Endo
@@ -68,8 +70,8 @@ runGenericValue (GenericValue val) = val
 -- | Smart constructor for `GenericValue`. Would return `Nothing` if given
 -- | `GenericSpine` doesn't conform to given `GenericSignature`
 genericValue :: GenericSignature -> GenericSpine -> Maybe GenericValue
-genericValue sig spine
-  | isValidSpine sig spine = Just $ GenericValue {signature: sig, spine: spine}
+genericValue signature spine
+  | isValidSpine signature spine = Just $ GenericValue { signature, spine }
   | otherwise = Nothing
 
 instance arbitraryGenericValue :: Arbitrary GenericValue where
@@ -80,44 +82,68 @@ instance arbitraryGenericValue :: Arbitrary GenericValue where
 
 -- | Generates `GenericSignature`s. Size parameter affects how nested the structure is.
 genGenericSignature :: Size -> Gen GenericSignature
-genGenericSignature size | size > 5 = genGenericSignature 5
-genGenericSignature 0 = elements SigNumber
-                                 (fromFoldable [ SigInt, SigString, SigBoolean ])
-genGenericSignature size = resize (size - 1) $ oneOf sigArray [sigProd, sigRecord]
+genGenericSignature size
+  | size > 5 = genGenericSignature 5
+  | size == 0 =
+      elements SigNumber $ fromFoldable [ SigInt, SigString, SigBoolean ]
+  | otherwise = resize (size - 1) $ oneOf sigArray [ sigProd, sigRecord ]
   where
-    sigArray = SigArray <<< const <$> sized genGenericSignature
+    sigArray = SigArray <<< defer <$> sized genGenericSignature
     sigRecord = do
       labels <- nub <$> arrayOf arbitrary
-      values <- arrayOf (const <$> sized genGenericSignature)
-      pure $ SigRecord $ zipWith { recLabel: _, recValue: _ } labels values
+      values <- arrayOf (defer <$> sized genGenericSignature)
+      let fields = zipWith { recLabel: _, recValue: _ } labels values
+      pure $ SigRecord fields
     sigProd = do
-      typeConstr <- arbitrary
-      constrs <- nub <$> arrayOf arbitrary
+      typeConstructor <- arbitrary
+      constructors <- nub <$> arrayOf arbitrary
       values  <- arrayOf (arrayOf (const <$> sized genGenericSignature))
-      pure $ SigProd typeConstr $ zipWith { sigConstructor: _, sigValues: _ } constrs values
+      let dataConstructors = zipWith { sigConstructor: _, sigValues: _ }
+                               constructors values
+      pure $ SigProd typeConstructor dataConstructors
 
 -- | Generates `GenericSpine`s that conform to provided `GenericSignature`.
 genGenericSpine :: GenericSignature -> Gen GenericSpine
-genGenericSpine = genGenericSpine' empty
+genGenericSpine = map force <<< genGenericSpine' mempty
 
-genGenericSpine' :: Array String -> GenericSignature -> Gen GenericSpine
-genGenericSpine' trail SigBoolean     = SBoolean <$> arbitrary
-genGenericSpine' trail SigNumber      = SNumber  <$> arbitrary
-genGenericSpine' trail SigInt         = SInt     <$> arbitrary
-genGenericSpine' trail SigString      = SString  <$> arbitrary
-genGenericSpine' trail SigChar        = SChar    <$> arbitrary
-genGenericSpine' trail SigUnit        = pure SUnit
-genGenericSpine' trail (SigArray sig) = SArray   <$> arrayOf (const <$> genGenericSpine' trail (sig unit))
-genGenericSpine' trail (SigProd _ sigs) = do
-  alt =<< maybe empty (\cons -> frequency cons.head (fromFoldable cons.tail))
-                      (uncons $ map (map pure) ctors)
+genGenericSpine' :: Array String -> GenericSignature -> Gen (Unit -> GenericSpine)
+genGenericSpine' trail SigBoolean     = defer <<< SBoolean <$> arbitrary
+genGenericSpine' trail SigNumber      = defer <<< SNumber  <$> arbitrary
+genGenericSpine' trail SigInt         = defer <<< SInt     <$> arbitrary
+genGenericSpine' trail SigString      = defer <<< SString  <$> arbitrary
+genGenericSpine' trail SigChar        = defer <<< SChar    <$> arbitrary
+genGenericSpine' trail SigUnit        = pure $ defer SUnit
+genGenericSpine' trail (SigArray sig) =
+  defer <<< SArray <$> arrayOf (genGenericSpine' trail (force sig))
+genGenericSpine' trail (SigProd _ constructors) = defer <$> do
+  { sigConstructor, sigValues } <- fold (frequencyWith probability <$> uncons constructors)
+  spines <- for sigValues (force >>> genGenericSpine' (sigConstructor : trail))
+  pure $ SProd sigConstructor spines
   where
-    trailCount sig = length $ filter ((==) sig.sigConstructor) trail
-    probability sig = 6.0 / (5.0 + toNumber (trailCount sig))
-    ctors = sigs <#> (\sig -> Tuple (probability sig) sig)
-    alt altSig = SProd altSig.sigConstructor
-                   <$> traverse (map const <<< genGenericSpine' (altSig.sigConstructor : trail) <<< (unit # _))
-                                altSig.sigValues
+    probability a = 6.0 / (5.0 + toNumber (trailCount a))
+      where
+        -- | How many times the given `DataConstructor` has already been used
+        trailCount :: DataConstructor -> Int
+        trailCount { sigConstructor } = length $ filter (eq sigConstructor) trail
 genGenericSpine' trail (SigRecord fieldSigs) =
-  SRecord <$> for fieldSigs \field -> do val <- genGenericSpine' trail (field.recValue unit)
-                                         pure $ field { recValue = const val }
+  defer <<< SRecord <$> for fieldSigs \fieldSig -> do
+    val <- genGenericSpine' trail $ force fieldSig.recValue
+    pure $ fieldSig { recValue = val }
+
+frequencyWith :: forall a f. (Functor f, Foldable f)
+              => (a -> Number)
+              -> { head :: a, tail :: f a }
+              -> Gen a
+frequencyWith probabilityFn { head, tail } =
+  frequency
+    (second pure $ annotate probabilityFn head)
+    (fromFoldable $ map (second pure <<< annotate probabilityFn) tail)
+
+annotate :: forall a b. (a -> b) -> a -> Tuple b a
+annotate f a = Tuple (f a) a
+
+defer :: forall a b. a -> (b -> a)
+defer = const
+
+force :: forall a. (Unit -> a) -> a
+force a = a unit
